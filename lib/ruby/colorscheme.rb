bb983bf31051c96dd2ff0b9dc16891640e7eeae3
James T. Lee <james@james.tl>
2017-01-07 01:55:36 -0500

    colorscheme: Massive refactor to account for bold
    I want better control over colors on low-color terminals like the linux
    console.  To do that I need to know when to enable bold to be able to
    access (especially) bright white and bright black.  This move more data
    into the Color class and tries to enforce a more functional style.

--- lib/ruby/colorscheme.rb
+++ lib/ruby/colorscheme.rb
@@ -4,12 +4,16 @@ class ColorScheme
   class Color
     attr_reader :hex, :hexr, :hexg, :hexb
     attr_reader :r, :g, :b
+    attr_reader :base, :ansi, :bold
 
-    def initialize(colorscheme, hex)
+    def initialize(colorscheme, hex, base, ansi, bold)
       @colorscheme = colorscheme
       @hex = hex
       (@hexr, @hexg, @hexb) = hex.scan(/../)
       (@r, @g, @b) = hex.scan(/../).map { |color| color.to_i(16) }
+      @base = base
+      @ansi = ansi
+      @bold = bold
     end
 
     # Modified from the original base16 builder
@@ -26,19 +30,19 @@ class ColorScheme
         (color.r * w1 + r * w2).to_i,
         (color.g * w1 + g * w2).to_i,
         (color.b * w1 + b * w2).to_i,
-      ])
+      ], nil, nil, nil)
     end
 
     # Add a bit of 'white' to a color.  White is defined by color 7, the end of the
     # gray scale in a color scheme.  (This is usually #FFFFFF, or close.)
     def tint(weight = 50)
-      mix(@colorscheme.bases['07'], weight)
+      mix(@colorscheme.colors_by_base['07'], weight)
     end
 
     # Add a bit of 'black' to a color.  White is defined by color 0, the end of the
     # gray scale in a color scheme.  (This is usually #000000, or close.)
     def shade(weight = 50)
-      mix(@colorscheme.bases['00'], weight)
+      mix(@colorscheme.colors_by_base['00'], weight)
     end
 
     def x11
@@ -50,68 +54,70 @@ class ColorScheme
     end
   end
 
+  # Map the base16 code to ansi number based on the number of available
+  # colors.  Where approximations had to be made, they were done subjectively
+  # by look (https://chriskempson.github.io/base16/) and style guidelines
+  # (https://github.com/chriskempson/base16/blob/master/styling.md).
   BASE_TO_ANSI = {
-    256 => ['00', '08', '0B', '0A', '0D', '0E', '0C', '05',
-            '03', '08', '0B', '0A', '0D', '0E', '0C', '07',
-            '09', '0F', '01', '02', '04', '06'],
-    16  => ['00', '08', '0B', '0A', '0D', '0E', '0C', '05',
-            '03', '08', '0B', '0A', '0D', '0E', '0C', '07',
-            '0A', '08', '00', '00', '05', '07'],
-    8   => ['00', '08', '0B', '0A', '0D', '0E', '0C', '05',
-            '03', '08', '0B', '0A', '0D', '0E', '0C', '07',
-            '0A', '08', '00', '00', '05', '05'],
+          # 00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F
+    256 => [ 0, 18, 19,  8, 20,  7, 21, 15,  1, 16,  3,  2,  6,  4,  5, 17],
+    16  => [ 0,  0,  8,  8,  8,  7, 15, 15,  1,  3,  3,  2,  6,  4,  5,  8],
+    8   => [ 0,  0,  0,'0',  7,  7,'7',  7,  1,  3,  3,  2,  6,  4,  5,'0'],
+                      # quoted means bold; as in "sorta 0", "sorta 7" ;)
   }
-
-  attr_reader :bases
 
   def initialize
     @scheme_data = YAML.load_file(File.join(Dir.home, '.colorscheme.yaml'))
-    @bases = (0x0..0xf).each_with_object(Hash.new) do |i, acc|
-      code = "%02X" % i
-      acc[code] = Color.new(self, @scheme_data["base#{code}"])
-      acc
+    @colors = BASE_TO_ANSI.keys.each_with_object(Hash.new) do |term_colors, colors_acc|
+      colors_acc[term_colors] = BASE_TO_ANSI[term_colors].each_with_index.map do |ansi, base|
+        basestr = '%02X' % base
+        hex = @scheme_data["base#{basestr}"]
+        bold = ansi.is_a? String
+        Color.new(self, hex, basestr, ansi.to_i, bold)
+      end
+      colors_acc
     end
-  end
-
-  def base_to_ansi(term_colors = 256)
-    canonical_mapping = BASE_TO_ANSI[256]
-    bases.keys.each_with_object(Hash.new) do |canonical_code, acc|
-      index = canonical_mapping.find_index(canonical_code)
-      actual_code = BASE_TO_ANSI[term_colors][index]
-      acc[canonical_code] = BASE_TO_ANSI[term_colors].find_index(actual_code) % term_colors
-      acc
-    end
-  end
-
-  def base_to_color(term_colors = 256)
-    mapping = Hash.new
-    base_to_ansi(term_colors).each do |code, ansi|
-      mapping[code] = colors(term_colors)[ansi]
-    end
-    mapping
   end
 
   def colors(term_colors = 256)
-    BASE_TO_ANSI[term_colors].map { |code| bases[code] }
+    @colors[term_colors]
+  end
+
+  def colors_by_ansi(term_colors = 256)
+    @colors[term_colors].sort do |c1, c2|
+      if c1.ansi == c2.ansi
+        c1.base <=> c2.base
+      else
+        c1.ansi <=> c2.ansi
+      end
+    end.each_with_object(Hash.new) do |color, acc|
+      acc[color.base] = color
+    end
+  end
+
+  def colors_by_base(term_colors = 256)
+    colors(term_colors).sort_by { |color| color.base }.each_with_object(Hash.new) do |color, acc|
+      acc[color.base] = color
+    end
   end
 
   def terminal
-    colors(256)
+    colors_by_ansi(256).values
   end
 
   def console
-    colors(8).take(16)
+    colors_by_ansi(8).values.take(16)
   end
 
   def foreground
-    bases['05']
+    colors_by_base['05']
   end
 
   def background
-    bases['00']
+    colors_by_base['00']
   end
 
   def cursor
-    bases['05']
+    colors_by_base['05']
   end
 end
