98363f5970cf0708420c20ea83a6b7fd1952cbed
James T. Lee <james@james.tl>
2019-01-15 01:35:20 -0500

    taffybar-status: ZSH-ize library

--- .config/taffybar/status/lib.zsh
+++ .config/taffybar/status/lib.zsh
@@ -5,7 +5,7 @@ STATE_NAME="_STATUS_${STATUS_NAME}_STATE"
 get_state() {
     local state="$(xprop -name "$TAFFYBAR_NAME" "$STATE_NAME" | awk -F '"' '/=/ { print $2 }')"
     [[ $state == '' ]] && return 1
-    echo "$state"
+    print "$state"
 }
 
 set_state() {
@@ -18,20 +18,20 @@ colorize() {
     local fg="$1"; shift
     local bg="$1"; shift
 
-    echo -n "<span fgcolor='$fg' bgcolor='$bg'>$@</span>"
+    print "<span fgcolor='$fg' bgcolor='$bg'>$@</span>"
 }
 
 fontawesome() {
     local glyph="$1"
-    echo -n "<span font='FontAwesome 5 Free Solid'>${glyph}</span>"
+    print "<span font='FontAwesome 5 Free Solid'>${glyph}</span>"
 }
 
 first_segment() {
-    echo "$(colorize '#282828' '#000000' '')$(colorize '#282828' '#282828' '█')$(colorize '#d8d8d8' '#282828' "$@ ")"
+    print "$(colorize '#282828' '#000000' '')$(colorize '#282828' '#282828' '█')$(colorize '#d8d8d8' '#282828' "$@ ")"
 }
 
 segment() {
-    echo "$(colorize '#000000' '#282828' '')$(colorize '#d8d8d8' '#282828' " $@ ")"
+    print "$(colorize '#000000' '#282828' '')$(colorize '#d8d8d8' '#282828' " $@ ")"
 }
 
 format_number() {
