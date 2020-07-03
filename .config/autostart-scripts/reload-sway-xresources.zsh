bb52841dd7f6297513ad5e8e48d145efa1ad9770
James T. Lee <james@james.tl>
2020-07-02 21:16:09 -0400

    reload-sway-xresources.zsh -> x-resources.service

--- .config/autostart-scripts/reload-sway-xresources.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-#
-# reload-sway-xresources.zsh
-# Plasma blows away the existing xrdb, so we have to reload it explicitly
-#
-
-if [[ $XDG_SESSION_TYPE == 'wayland' ]]; then
-    xrdb -merge /etc/sway/Xresources
-fi
