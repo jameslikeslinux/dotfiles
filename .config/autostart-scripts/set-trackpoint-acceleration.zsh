ee7eb7c6ed1c716229b537ca547a6453c341e0bc
James T. Lee <james@james.tl>
2020-07-02 22:12:32 -0400

    Remove obsolete set-trackpoint-acceleration.zsh autostart-script
    If I need this again, it should be implemented as a systemd service.

--- .config/autostart-scripts/set-trackpoint-acceleration.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-#
-# set-trackpoint-acceleration.zsh
-# Plasma overrides the system trackpoint acceleration, so set it explicitly
-#
-
-if [[ $XDG_SESSION_TYPE == 'x11' ]]; then
-    xinput set-prop 'TPPS/2 IBM TrackPoint' 'libinput Accel Speed' "$(awk '/TrackPoint/ { trackpoint = 1 } /AccelSpeed/ && trackpoint { print gensub(/"/, "", "g", $3) }' /etc/X11/xorg.conf.d/10-libinput.conf)"
-fi
