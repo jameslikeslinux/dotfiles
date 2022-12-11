57975882ef42948e90ffce3a79b34fb05ae9c619
James Lee <james@james.tl>
2022-12-11 02:00:45 -0500

    systemd: Make xmonad behave like sway
    xmonad is launched directly from greetd rather than having Plasma launch
    it from SDDM.

--- .config/plasma-workspace/env/refresh-session.sh
+++
@@ -1,7 +0,0 @@
-#
-# refresh-session.sh
-# Clean up any lingering environment variables from a previous session
-#
-
-source "${HOME}/lib/session-vars.sh"
-systemctl --user unset-environment "${SESSION_VARS[@]}"
--- .config/plasma-workspace/shutdown/stop-plasma-session-target.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# stop-graphical-session-target.sh
-# Stop the systemd target that controls graphical-session.target
-#
-
-systemctl --user stop plasma-session.target
