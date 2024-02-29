ef3af971482459fd1498be152043a14731d73e04
James Lee <james@james.tl>
2024-02-28 19:24:29 -0500

    lib: Clear out old dotfiles class
    Trying to address unchecked use of the `profile` fact.

--- lib/puppet/dotfiles/manifests/init.pp
+++ lib/puppet/dotfiles/manifests/init.pp
@@ -1,11 +1,3 @@
 class dotfiles {
-  $urxvtd_ensure = $facts['profile']['platform'] ? {
-    'pinebookpro' => absent,
-    default       => absent,
-  }
-
-  file { "${facts['basedir']}/.config/systemd/user/x-session.target.wants/urxvtd.service":
-    ensure => $urxvtd_ensure,
-    target => '../urxvtd.service',
-  }
+  # empty
 }
