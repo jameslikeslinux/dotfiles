cad14c57470c47d2e90897e41c5ded995949a377
James Lee <james@james.tl>
2024-09-19 23:52:19 -0400

    pinebookpro: Restore legacy terminals
    Alacritty is dog on panfrost. On X, urxvt isn't much better. xterm is
    actually pretty good. On Wayland, foot is very good. Thankfully I have
    many alterantives and no strong preference :)

--- lib/puppet/dotfiles/manifests/init.pp
+++ lib/puppet/dotfiles/manifests/init.pp
@@ -1,3 +1,17 @@
 class dotfiles {
-  # empty
+  if $facts['profile']['platform'] == 'pinebookpro' {
+    file { "${facts['basedir']}/.config/systemd/user/wayland-session.target.wants":
+      ensure => directory,
+    }
+
+    file { "${facts['basedir']}/.config/systemd/user/wayland-session.target.wants/foot.service":
+      ensure => link,
+      target => '../foot.service',
+    }
+  } else {
+    file { "${facts['basedir']}/.config/systemd/user/wayland-session.target.wants":
+      ensure => absent,
+      force  => true,
+    }
+  }
 }
