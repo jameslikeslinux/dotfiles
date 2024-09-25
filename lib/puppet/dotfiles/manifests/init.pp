ad3618020149d82f1f1da487c41326737c189abd
James Lee <james@james.tl>
2024-09-25 02:13:59 -0400

    templates: Fix build when profile is nil

--- lib/puppet/dotfiles/manifests/init.pp
+++ lib/puppet/dotfiles/manifests/init.pp
@@ -1,5 +1,5 @@
 class dotfiles {
-  if $facts['profile']['platform'] == 'pinebookpro' {
+  if $facts['profile'] and $facts['profile']['platform'] == 'pinebookpro' {
     file { "${facts['basedir']}/.config/systemd/user/wayland-session.target.wants":
       ensure => directory,
     }
