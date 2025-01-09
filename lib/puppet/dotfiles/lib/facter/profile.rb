71b747ccebab3928b5d59fab2e1452e42ea2a7a0
James Lee <james@james.tl>
2025-01-09 13:40:48 -0500

    facter: Update profile
    Rename role -> variant

--- lib/puppet/dotfiles/lib/facter/profile.rb
+++ lib/puppet/dotfiles/lib/facter/profile.rb
@@ -5,9 +5,9 @@ Facter.add('profile') do
     profile = Facter::Core::Execution.execute('/usr/bin/eselect --brief profile show')
     case profile
     when %r{nest:(\S+)/(\S+)/(\S+)}
-      { architecture: arch, cpu: Regexp.last_match(1), platform: Regexp.last_match(2), role: Regexp.last_match(3) }
+      { architecture: arch, cpu: Regexp.last_match(1), platform: Regexp.last_match(2), variant: Regexp.last_match(3) }
     when %r{nest:(\S+)/(\S+)}
-      { architecture: arch, cpu: Regexp.last_match(1), platform: Regexp.last_match(1), role: Regexp.last_match(2) }
+      { architecture: arch, cpu: Regexp.last_match(1), platform: Regexp.last_match(1), variant: Regexp.last_match(2) }
     end
   end
 end
